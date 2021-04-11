a844c5f980f4c529de8b2a4c8e2e9208644ccbec
chris <c.caressimo@gmail.com>
2021-04-11 17:50:12 -0400

    added fetch, fetched from api, printing data

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
-var config = {
+module.exports = {
   WEATHER_API_KEY: "63523f8b1d9eec875e8cd5b773a249f4",
 };
--- server.js
+++ server.js
@@ -1,5 +1,9 @@
+const config = require('./config')
+const apiKey = config.WEATHER_API_KEY
+
 const express = require('express')
 const bodyParser = require('body-parser')
+const fetch = require('node-fetch')
 const app = express()
 
 app.use(express.static('public'))
@@ -13,8 +17,25 @@ app.get('/', (req, res) => {
 })
 
 app.post("/", (req, res) => {
-  res.render("index");
-  console.log(req.body.city);
+    let city = req.body.city;
+    let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`;
+    console.log(url);
+    fetch(url)
+        .then(res => res.json())
+        .then(data => {
+                let weather = data
+                if(weather.main == undefined){
+                    res.render('index', {weather: null, error: 'Error, please try again'})
+                } else {
+                    let weatherText = ` It's ${weather.main.temp} degrees in ${weather.name}!`
+                    res.render('index', {weather: weatherText, error: null})
+                
+                }
+        })
+
+
+    //   res.render("index");
+//   console.log(req.body.city);
 });
 
 app.listen(port, () => {
