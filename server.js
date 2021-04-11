0e29460da70f406f46abd557d740bc39de4783df
chris <c.caressimo@gmail.com>
2021-04-11 17:12:23 -0400

    added css and setting up

--- server.js
+++ server.js
@@ -1,13 +1,21 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const app = express()
 
+app.use(express.static('public'))
+app.use(bodyParser.urlencoded({extended: true}))
 app.set('view engine', 'ejs')
 
 const port = 3000
 
 app.get('/', (req, res) => {
-    res.send('<h1>Hello world!<h1>')
+    res.render('index')
 })
+
+app.post("/", (req, res) => {
+  res.render("index");
+  console.log(req.body.city);
+});
 
 app.listen(port, () => {
     console.log(`server is running at http://localhost:${port}`);
